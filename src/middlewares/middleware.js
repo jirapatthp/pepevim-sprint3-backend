887fe727364e62af2b9c13be96541390ce76f718
colddcereal <jirapatthepsurin@192.168.1.4>
2026-01-17 01:43:32 +0700

    feat: implement user authentication and create user functionality

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { env } from "../utils/env.js";
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.accessToken;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized. No token provided." });
+    }
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+    req.user = { userId: decoded.userId };
+    
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
--- src/routes/v1/contact.route.js
+++ src/routes/v1/contact.route.js
@@ -6,3 +6,5 @@ const router = express.Router();
 router.post("/", contactController.createContact);
 
 export default router;
+
+
